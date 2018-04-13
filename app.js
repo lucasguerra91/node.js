48049ad2b6b9e3a6251bd26b7ecf4b00499bfa25
Lucas Guerra <guerra986@gmail.com>
2018-04-13 15:52:18 -0300

    mostrando lista de tablas

--- app.js
+++ app.js
@@ -2,17 +2,10 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-<<<<<<< HEAD
-=======
 //var favicon = require('serve-favicon');
->>>>>>> 895b119551b9685575b04a7aa8a041ebddafd861
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
-
-// Seteando el router para la Wiki
-var wiki = require('./wiki');
 
 
 
@@ -30,14 +23,13 @@ db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 // Estos archivos contienen codigo para manejar las rutas
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var catalog = require('./routes/catalog'); //Import routes for "catalog" area of site
+
+// Seteando el router para la Wiki, solo practica
+var wiki = require('./wiki');
 
 // Creacion del objeto app usando el modulo express
 var app = express();
-
-
-// A partir de aca puedo usar use()
-app.use('/wiki', wiki);
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -54,19 +46,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Se define el uso de los manejadores de rutas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// A partir de aca puedo usar use()
+app.use('/wiki', wiki);
+app.use('/catalog', catalog); // Add catalog routes to middleware chain.
 
-// catch 404 and forward to error handler
+
+
+
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
+    // Set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
+    // Render the error page
     res.status(err.status || 500);
     res.render('error');
 });
