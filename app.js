4d8ae7ec10ab99afb2abc77c8e1d8a551842b1c4
Lucas Guerra <guerra986@gmail.com>
2018-04-13 09:08:58 -0300

    para pullear

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -18,7 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))  <-- Aca iria nuestro icono 
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))  <-- Aca iria nuestro icono
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
